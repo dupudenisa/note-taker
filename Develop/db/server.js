9ca8cd47119d8fc3c89c888f601a8b0e8b3d7e19
Denisa Dupu <dupudenisa@gmail.com>
2020-03-06 19:51:42 -0600

    redo last commit

--- Develop/db/server.js
+++ Develop/db/server.js
@@ -12,12 +12,12 @@ app.use(express.static('public'));
 
 
 app.get("/notes",function(req,res){
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 })
 
 
 app.get("/index",function(req,res){
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 })
 
 
